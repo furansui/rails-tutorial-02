91a5e9a677510434cecd7b24a231eee4cccb65f1
furansui <furansui@gmail.com>
2014-03-03 12:58:37 +0900

    Initial commit, not yet bundled

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsTutorial02::Application.config.secret_key_base = '5fa65a2fe448a45df9bf07e6785906fa941f7269aea53d6977e5e3676ca1a025381d0eecb917409c6f083e411bd677dac849a50ca7f424ec68c35751b38d6a70'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsTutorial02::Application.config.session_store :cookie_store, key: '_rails-tutorial-02_session'
